0eff35c6477d80aa0558fdfc5b68bb8bc3a16b2c
João Vitor <39197618+ancogamer@users.noreply.github.com>
2021-01-18 08:39:15 -0300

    Start struct of the cruds

---
+++ controller/route/route.go
@@ -0,0 +1,22 @@
+package route
+
+import (
+	"github.com/gofiber/cors"
+	"github.com/gofiber/fiber"
+	mw "github.com/gofiber/fiber/middleware"
+)
+
+//
+func AllRoutes(db dbStruct, app *fiber.App) {
+	////
+	app.Use(cors.New())
+	app.Use(mw.Compress(mw.CompressLevelBestSpeed))
+	app.Use(mw.Logger("${time} ${method} ${path} - ${ip} - ${status} - ${latency}\n"))
+
+	///
+
+	// application/json
+	// application/x-www-form-urlencoded
+	// multipart/form-data
+
+}
