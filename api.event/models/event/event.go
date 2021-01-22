84cbe3e3ced7407ddb26969fb4248255c41a7bca
João Vitor <39197618+ancogamer@users.noreply.github.com>
2021-01-22 08:17:18 -0300

    fix table comments

--- api.event/models/event/event.go
+++ api.event/models/event/event.go
@@ -16,20 +16,22 @@ type Event struct {
 }
 
 /*
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE  events (
 	ev_id serial not null,
 	ev_uuid uuid default uuid_generate_v4(),
-	ev_data_create date not null default now,
-	ev_date_update date not null default now,
+	ev_data_create date not null default now(),
+	ev_date_update date not null default now(),
 	ev_name character varying(300) not null,
 	ev_user_create_uuid uuid not null,
 	ev_data_init date not null,
 	ev_data_end date not null,
-	ev_hour_init time without timezone not null,
-	ev_hour_end time without timezone not null,
+	ev_hour_init time without time zone not null,
+	ev_hour_end time without time zone not null,
 	ev_qtd_max integer not null,
 	ev_qtd_now integer,
-	ev_qtd_min integer not null default 0
+	ev_qtd_min integer not null default 0,
 
 	CONSTRAINT  ev_pkey PRIMARY KEY(ev_uuid)
 )
